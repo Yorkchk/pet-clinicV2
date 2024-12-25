315fde259011da71adcf0c95d959b9f607d9a908
Youssef Cherkaoui <youssef.cherkaoui1234@gmail.com>
2024-12-25 18:23:48 -0500

    first commit

---
+++ src/test/java/com/example/pet_clinicV2/PetClinicV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pet_clinicV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
