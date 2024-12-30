b511c6b69013785002887490d45e6fbfefcc35dd
Youssef Cherkaoui <youssef.cherkaoui1234@gmail.com>
2024-12-29 23:16:26 -0500

    added the form

--- src/main/java/com/example/pet_clinicV2/controllers/OwnerController.java
+++ src/main/java/com/example/pet_clinicV2/controllers/OwnerController.java
@@ -1,10 +1,12 @@
 package com.example.pet_clinicV2.controllers;
 
+import com.example.pet_clinicV2.models.Owner;
 import com.example.pet_clinicV2.serviceInt.OwnerInt;
 import com.example.pet_clinicV2.services.OwnerImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -28,5 +30,14 @@ public class OwnerController {
         return "searchOwner";
     }
 
+    @RequestMapping("addOwner")
+    public String addOwner(){
+        return "createOwner";
+    }
 
+    @RequestMapping("saveOwner")
+    public String saveOwner(@ModelAttribute("owner") Owner owner){
+        ownerInt.addOwner(owner);
+        return "home";
+    }
 }
